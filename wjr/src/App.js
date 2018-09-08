1f890f39300c8d9fe7a1020f10e25766d5ce7da5
Jesus-Romero <ejesusromero@gmail.com>
2018-09-08 11:46:05 -0500

    Dev 0.0.1 : Backgrounds Section Implemented

--- wjr/src/App.js
+++ wjr/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from './components/Home';
+import Background from './components/Background'
 
 class App extends Component {
   render() {
     return (
       <div>
       	<Home />
+        <Background />
       </div>
     );
   }
---
+++ wjr/src/components/Background.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Background.css';
+
+class Background extends Component {
+  render() {
+    return (
+      <div>
+          <div className="section-titles section-titles-c">
+            <h3>Background</h3>
+          </div>
+          <div className="section-text section-text-c">
+            <h5>Hello, My Skills include</h5>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default Background;
