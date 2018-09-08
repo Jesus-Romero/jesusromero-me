6e79a40daba2f69479d11db729928fd278e82d4a
root <root@ip-172-31-17-232.us-east-2.compute.internal>
2018-09-08 15:04:07 +0000

    Development 0.0.1 : Name and Title Implemented

--- wjr/src/components/Home.jsx
+++ wjr/src/components/Home.jsx
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap';
 import './Home.css';
 
 class Home extends Component {
   render() {
     return (
-      <div className="Home">
-        <header className="Home-header">
-          <h1 className="My-name">Jesus Romero</h1>
+      <div>
+        <header>
+          <h1 class="Home-name Home-name-a">Jesus Romero</h1>
         </header>
-        <h3 className="Personal-intro">
-          Current Title: Recent Graduate
-        </h3>
+        <p className="Home-title Home-title-a">
+          <b>Current Title:</b> Recent Graduate
+        </p>
       </div>
     );
   }
 }
 
-export default App;
+export default Home;
