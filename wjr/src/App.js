359e4d5108846e0f60139f4c26483c1bf654de65
Jesus-Romero <ejesusromero@gmail.com>
2018-09-12 20:31:38 -0500

    Dev 0.0.1 - Completed (- side-navbar)

--- wjr/src/App.js
+++ wjr/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 import './App.css';
 import Home from './components/Home';
 import About from './components/About';
@@ -9,11 +11,11 @@ class App extends Component {
   render() {
     return (
       <div>
-      	<Home />
-        <About />
-        <Skills />
-        <Projects />
-      </div>
+    	<Home/>
+      <About/>
+      <Skills/>
+      <Projects/>
+    </div>
     );
   }
 }
--- wjr/src/components/Projects.jsx
+++ wjr/src/components/Projects.jsx
@@ -11,10 +11,10 @@ class Projects extends Component {
       </div>
       <div className="block">
         <div className="section-text">
+          <div className="projects-title">
+            <b className="section-title-c">pocketchef.me</b>
+          </div>
           <div className="projects-title-tools">
-            <div className="projects-title">
-              <b className="section-title-c">pocketchef.me</b>
-            </div>
             <font className="section-title-c projects-tools">React</font>
             <font className="section-title-c projects-tools">Python</font>
             <font className="section-title-c projects-tools">SQL</font>
@@ -30,30 +30,51 @@ class Projects extends Component {
            Presented visitors with restaurant information and recipe information fetched from pocketchef's Flask-Restless generated API with API requests.
            Front-End implemented the user interface with Reactstrap and supported pagination, filtering, and searching of the data.</font>
           </div>
-        </div>
 
-        <div className="section-text">
-          <b className="section-title-c">Dex</b>
-          <div className="projects-description">
-            <font className="section-text-c">&emsp;&emsp;&emsp;An Android application, emulating a Pokedex, which presents all Pokemon's information,
-            fetched asynchronously from PokeAPI using threads and stored locally in a database. Users can filter the Pokemon database,
-            login with validated credentials with the help of FirebaseAuth, and store custom teams in a Firebase Database instance and send those teams to other users.</font>
+
+          <div className="projects-title">
+            <b className="section-title-c">Dex</b>
           </div>
-        </div>
-
-        <div className="section-text">
-          <b className="section-title-c">Pintos</b>
-          <div className="projects-description">
-            <font className="section-text-c">&emsp;&emsp;&emsp;A "pint-sized" operating system built upon a basic instructional x86 operating system that supports priority scheduling,
-            argument passing on the stack, system calls for user programs, virtual memory, and a multi-level indexed file system.</font>
+          <div className="projects-title-tools">
+            <font className="section-title-c projects-tools">Java</font>
+            <font className="section-title-c projects-tools">SQLite</font>
+            <font className="section-title-c projects-tools">Firebase</font>
+            <font className="section-title-c projects-tools">XML</font>
           </div>
-        </div>
 
-        <div className="section-text">
-          <b className="section-title-c">Compiler</b>
           <div className="projects-description">
-            <font className="section-text-c">&emsp;&emsp;&emsp;A compiler for the Pascal language that generates assembly code with the use of a lexical analyzer that converts
-            characters into tokens, a parser that analyzes Pascal statements with the use of a defined grammar and actions, and a code generator.</font>
+           <font className="section-text-c">&emsp;&emsp;&emsp;An Android application, emulating a Pokedex, which presents all Pokemon's information,
+           fetched asynchronously from PokeAPI using threads and stored locally in a database. Users can filter the Pokemon database,
+           login with validated credentials with the help of FirebaseAuth, and store custom teams in a Firebase Database instance and send those teams to other users.</font>
+          </div>
+
+
+          <div className="projects-title">
+            <b className="section-title-c">Pintos</b>
+          </div>
+          <div className="projects-title-tools">
+            <font className="section-title-c projects-tools">C</font>
+            <font className="section-title-c projects-tools">Git</font>
+          </div>
+
+          <div className="projects-description">
+           <font className="section-text-c">&emsp;&emsp;&emsp;A "pint-sized" operating system built upon a basic instructional x86 operating system that supports priority scheduling,
+           argument passing on the stack, system calls for user programs, virtual memory, and a multi-level indexed file system.</font>
+          </div>
+
+
+          <div className="projects-title">
+            <b className="section-title-c">Compiler</b>
+          </div>
+          <div className="projects-title-tools">
+            <font className="section-title-c projects-tools">C</font>
+            <font className="section-title-c projects-tools">Yacc</font>
+            <font className="section-title-c projects-tools">Lex</font>
+          </div>
+
+          <div className="projects-description">
+           <font className="section-text-c">&emsp;&emsp;&emsp;A compiler for the Pascal language that generates assembly code with the use of a lexical analyzer that converts
+           characters into tokens, a parser that analyzes Pascal statements with the use of a defined grammar and actions, and a code generator.</font>
           </div>
         </div>
       </div>
