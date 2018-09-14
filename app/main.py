c40a66aa0fdd48ff4f980f2d6a9af3782fd40f27
Jesus-Romero <ejesusromero@gmail.com>
2018-09-14 13:13:09 -0500

    Dev. 0.0.1 - Removed invalid dependencies from package.json

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, static_folder="../wjr/static", template_folder="../wjr/public")
+app = Flask(__name__)
 
 
 @app.route('/', defaults={'path': ''})
@@ -8,4 +8,4 @@ def index(path):
      return render_template('index.html');
 
 if __name__ == "__main__":
-     app.run(host='0.0.0.0', port=4000)
+     app.run(host='0.0.0.0', port=80)
