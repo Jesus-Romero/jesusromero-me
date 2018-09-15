098f22a17cea298a0de2565a300f7cce0ab18be7
Jesus-Romero <ejesusromero@gmail.com>
2018-09-15 14:56:51 -0500

    Dev. 0.1.1-3

--- wjr/src/components/Home.jsx
+++ wjr/src/components/Home.jsx
@@ -12,11 +12,11 @@ class Home extends Component {
         <b className="Home-title Home-currenttitle-a">Current Title:</b> <font className="Home-title Home-thetitle-a">Recent Graduate</font>
       </p>
       <div className="logo-block">
-        <a href="https://twitter.com/tJesusRomero"><img src="https://i.imgur.com/k6KKh2q.png" width="37" height="37" className="logo Home-twitter-a"/></a>
-        <a href="https://www.facebook.com/fJesusRomero"><img src="https://i.imgur.com/Mnti9Dc.png" width="37" height="37" className="logo Home-facebook-a"/></a>
-        <a href="https://www.github.com/Jesus-Romero"><img src="https://i.imgur.com/zWcYEFj.png" width="37" height="37" className="logo Home-github-a"/></a>
-        <a href="https://www.linkedin.com/in/inJesusRomero"><img src="https://i.imgur.com/jgg8tiS.png" width="37" height="37" className="logo Home-in-a"/></a>
-        <a href="mailto:ejesusromero@gmail.com"><img src="https://i.imgur.com/xThw5ov.png" width="37" height="37" className="logo Home-email-a"/></a>
+        <a href="https://twitter.com/tJesusRomero" target="_blank"><img src="https://i.imgur.com/k6KKh2q.png" width="37" height="37" className="logo Home-twitter-a"/></a>
+        <a href="https://www.facebook.com/fJesusRomero" target="_blank"><img src="https://i.imgur.com/Mnti9Dc.png" width="37" height="37" className="logo Home-facebook-a"/></a>
+        <a href="https://www.github.com/Jesus-Romero" target="_blank"><img src="https://i.imgur.com/zWcYEFj.png" width="37" height="37" className="logo Home-github-a"/></a>
+        <a href="https://www.linkedin.com/in/inJesusRomero" target="_blank"><img src="https://i.imgur.com/jgg8tiS.png" width="37" height="37" className="logo Home-in-a"/></a>
+        <a href="mailto:ejesusromero@gmail.com" target="_blank"><img src="https://i.imgur.com/xThw5ov.png" width="37" height="37" className="logo Home-email-a"/></a>
       </div>
     </div>
     );
